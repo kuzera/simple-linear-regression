bacdb4d08a2c682179dbf1bbd63f2ae6c948067f
Zbigniew Kuzera <kuzera@gmail.com>
2018-02-06 12:00:18 +0100

    make test pass

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
 import test from 'ava';
+import linearRegression from './';
 
 test('foo', t => {
-    t.pass();
+    const given = [[43, 99],
+        [21, 65],
+        [25, 79],
+        [42, 75],
+        [57, 87],
+        [59, 81]];
+
+    const expected = linearRegression(given);
+
+    t.deepEqual(expected, {
+        a: 65.1415715245131,
+        b: 0.38522498321020804
+    });
 });
